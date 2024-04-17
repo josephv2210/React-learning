6d604f60ec56ca63103d82d9a50a1b805720bce7
JosephVallejoBlockchainDevelop <josephvallejo@blockchaindc.co>
2024-04-17 15:49:08 -0500

    feat(mouse-follower)

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -4,6 +4,16 @@ const FollowMouse = () => {
   const [enabled, setEneabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  // pointer move
   useEffect(() => {
     console.log('effect', { enabled })
 
